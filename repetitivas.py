b2e4e93cc8a7db259d4d0b671df4f77c88fe0328
angerson25 <angerson2224639@correo.uis.edu.co>
2022-09-29 10:08:08 -0500

    Terminado

--- repetitivas.py
+++ repetitivas.py
@@ -1,5 +1,10 @@
+#INICIO
+print("\n---SUMA-N-PRIMEROS-NUMEROS---\n")
+
+#INPUT
 n=int(input("\nDigite el valor de los primeros numeros que desea sumar: "))
 
+#PROCESSING
 i=1
 sum=0
 
@@ -7,4 +12,7 @@ while i<=n:
     sum= sum + i
     i = i + 1
 
+
 print("\nLa suma de los primeros " + str(n) + " numeros es: " + str(sum))
+
+#INICIO
